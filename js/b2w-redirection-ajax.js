8455408100d8af5f75f6a2cdf3bd77a893bcbb19
Deepak Lalwani <deepak.lalwani81@gmail.com>
2019-03-01 17:50:07 +0530

    Fix phpcs errors

--- js/b2w-redirection-ajax.js
+++ js/b2w-redirection-ajax.js
@@ -1,20 +1,23 @@
-function rt_start_config( nonce ){ 
-    if(jQuery('#get_config').html()!=''){
-        jQuery('#get_config').html('');
-    }
-    jQuery.ajax({
-        url:'admin-ajax.php',
-        type: 'POST',
-        data: 'action=rt_b2wr_get_config&nonce='+nonce,
-        success: function(result){
-            jQuery('#get_config').append(result);
-        }
-    });
+function rt_start_config( nonce ){
+	if (jQuery( '#get_config' ).html() != '') {
+		jQuery( '#get_config' ).html( '' );
+	}
+	jQuery.ajax(
+		{
+			url:'admin-ajax.php',
+			type: 'POST',
+			data: 'action=rt_b2wr_get_config&nonce=' + nonce,
+			success: function(result){
+				jQuery( '#get_config' ).append( result );
+			}
+		}
+	);
 }
 
 function generate_code(num, domain_name, curr_domain, nonce){
-    jQuery('#re_code').remove();
-    jQuery('#code_here').html('<br/><h3><u>Generated Code</u></h3><div id ="re_code"><strong>Redirection code for <a href="http://'+domain_name+ '">'+domain_name+ '</a></strong><br/>\n\
+	jQuery( '#re_code' ).remove();
+	jQuery( '#code_here' ).html(
+		'<br/><h3><u>Generated Code</u></h3><div id ="re_code"><strong>Redirection code for <a href="http://' + domain_name + '">' + domain_name + '</a></strong><br/>\n\
         Copy template code generated below and paste them in your Blogger.com template. (<a href="http://rtcamp.com/tutorials/blogger-to-wordpress-redirection-plugin/" target="_blank">How to do this?</a>)<br/>\n\
         <span class="description">(<strong>Important:</strong> Do NOT forget to take a backup of old template code.)</span>\n\
         <p><textarea onclick="this.select()" cols="55" rows="12">\n\
@@ -25,11 +28,11 @@ function generate_code(num, domain_name, curr_domain, nonce){
 <b:include data=\'blog\' name=\'all-head-content\'/>\n\
 <title><data:blog.pageTitle&#47;></title>\n\
 <b:if cond=\'data:blog.pageType == &quot;item&quot;\'>\n\
-<link expr:href=\'&quot;'+curr_domain+'?b2w=&quot;+data:blog.url\' rel=\'canonical\'/>\n\
-<meta expr:content=\'&quot;0;url='+curr_domain+'?b2w=&quot;+data:blog.url\' http-equiv=\'refresh\'/>\n\
+<link expr:href=\'&quot;' + curr_domain + '?b2w=&quot;+data:blog.url\' rel=\'canonical\'/>\n\
+<meta expr:content=\'&quot;0;url=' + curr_domain + '?b2w=&quot;+data:blog.url\' http-equiv=\'refresh\'/>\n\
 <b:else/>\n\
-<link href=\''+curr_domain+'\' rel=\'canonical\'/>\n\
-<meta content=\'0;url='+curr_domain+'\' http-equiv=\'refresh\'/>\n\
+<link href=\'' + curr_domain + '\' rel=\'canonical\'/>\n\
+<meta content=\'0;url=' + curr_domain + '\' http-equiv=\'refresh\'/>\n\
 </b:if>\n\
 <b:skin>\n\
 <![CDATA[/*-----------------------------------------------\n\
@@ -45,50 +48,57 @@ Name: B2W\n\
 </b:section>\n\
 <b:if cond=\'data:blog.pageType == &quot;item&quot;\'>\n\
 <script type="text/javascript">\n\
-window.location.replace(&quot;'+curr_domain+'/?b2w=&quot;+encodeURI(window.location.protocol + "//" + window.location.host+window.location.pathname));\n\
+window.location.replace(&quot;' + curr_domain + '/?b2w=&quot;+encodeURI(window.location.protocol + "//" + window.location.host+window.location.pathname));\n\
 </script>\n\
 <b:else/>\n\
 <script type="text/javascript">\n\
-window.location.replace(&quot;'+curr_domain+'&quot;+window.location.pathname);\n\
+window.location.replace(&quot;' + curr_domain + '&quot;+window.location.pathname);\n\
 </script>\n\
 </b:if>\n\
 <div style=\'margin: 0 auto;text-align:center;\'> <h1>This Page</h1>\n\
 <p>has moved to a new address:</p>\n\
 <b:if cond=\'data:blog.pageType == &quot;item&quot;\'>\n\
-<a expr:href=\'&quot;'+curr_domain+'?b2w=&quot;+data:blog.url\'><data:blog.pageTitle&#47;></a>\n\
+<a expr:href=\'&quot;' + curr_domain + '?b2w=&quot;+data:blog.url\'><data:blog.pageTitle&#47;></a>\n\
 <b:else/>\n\
-<a href=\''+curr_domain+'\'>'+curr_domain+'</a>\n\
+<a href=\'' + curr_domain + '\'>' + curr_domain + '</a>\n\
 </b:if>\n\
 <p>Sorry for the inconvenience&hellip; </p>\n\
 Redirection provided by <a href="http://rtcamp.com/" title="Blogger to WordPress Migration Service">Blogger to WordPress Migration Service</a></div>\n\
 </body>\n\
 </html>\
 </textarea></p>\n\
-	After the redirection setup press <b>"Verify Configuration"</b> button below to test your configuration. <br />When you press the button it will generate a random link for a post on <b>'+ domain_name +'</b><div class="submit" style="padding-bottom: 0.5em !important"><input type="submit" class="button-primary" onclick = "check_configuration(\''+domain_name+'\',\''+nonce+'\')" name="start" id ="check_config" value="Verify Configuration"/><br /></div><div id ="verify_config"></div>');
+	After the redirection setup press <b>"Verify Configuration"</b> button below to test your configuration. <br />When you press the button it will generate a random link for a post on <b>' + domain_name + '</b><div class="submit" style="padding-bottom: 0.5em !important"><input type="submit" class="button-primary" onclick = "check_configuration(\'' + domain_name + '\',\'' + nonce + '\')" name="start" id ="check_config" value="Verify Configuration"/><br /></div><div id ="verify_config"></div>'
+	);
 }
 
 function check_configuration(domain_name, nonce){
-    console.log(nonce);
-    if(jQuery('#verify_config').html()!=''){
-        jQuery('#verify_config').html('');
-    }
-    jQuery.ajax({
-        url:'admin-ajax.php',
-        type: 'POST',
-        data: 'action=rt_b2wr_verify_config&dname='+domain_name+'&config_nonce='+nonce,
-        success: function(result){
-            jQuery('#verify_config').append(result);
-        }
-    });
+	console.log( nonce );
+	if (jQuery( '#verify_config' ).html() != '') {
+		jQuery( '#verify_config' ).html( '' );
+	}
+	jQuery.ajax(
+		{
+			url:'admin-ajax.php',
+			type: 'POST',
+			data: 'action=rt_b2wr_verify_config&dname=' + domain_name + '&config_nonce=' + nonce,
+			success: function(result){
+				jQuery( '#verify_config' ).append( result );
+			}
+		}
+	);
 }
 
-jQuery('#hide_b2wr_notice_block').click(function() {
-    jQuery.ajax({
-        url:'admin-ajax.php',
-        type: 'POST',
-        data: 'action=rt_b2wr_hide_notice_block',
-        success: function(result){
-            jQuery('#b2wr_notice_block').slideUp('slow','linear');
-        }
-    });
-});
+jQuery( '#hide_b2wr_notice_block' ).click(
+	function() {
+		jQuery.ajax(
+			{
+				url:'admin-ajax.php',
+				type: 'POST',
+				data: 'action=rt_b2wr_hide_notice_block',
+				success: function(result){
+					jQuery( '#b2wr_notice_block' ).slideUp( 'slow','linear' );
+				}
+			}
+		);
+	}
+);
