abf0daf13c64863ba81158f43db694d154f6e2dd
Deepak Lalwani <deepak.lalwani81@gmail.com>
2019-03-01 17:06:39 +0530

    Add template-update-notice.php -Moved update notice code to separate template file

---
+++ templates/template-update-notice.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Update notice template file
+ *
+ * @package blogger-to-wordpress
+ */
+
+?>
+
+<div id="b2wr_notice_block" class="error">
+	<p>Due to recent updates on blogger.com, Blogger to WordPress Redirection plugin has been rewritten. The process has also changed completely. Please refer the updated instructions here: 
+		<a class="blue_color" href="https://bloggertowp.org/tutorials/blogger-to-wordpress-redirection-plugin/" target="_blank" title="Read more details about this update at Our Blog">(Read More…)</a>
+		<span>
+		<input type="button" id="hide_b2wr_notice_block" value="Hide this message!" class="button">
+		</span>
+	</p>
+</div>';
