1182b997204cf266d42410648396857e4b470c10
devansh016 <devanshchaudhary2002@gmail.com>
2024-07-26 14:37:22 +0530

    Transalate strings and update .pot file

--- templates/blogger-code.php
+++ templates/blogger-code.php
@@ -10,17 +10,17 @@
 <br/>
 
 <h3>
-	<u>Generated Code</u>
+	<u><?php esc_html_e( 'Generated Code', 'blogger-to-wordpress-redirection' ); ?></u>
 </h3>
 
-<strong>Redirection code for <a id="redirection-domain-name-1" href=""></a></strong>
+<strong><?php esc_html_e( 'Redirection code for', 'blogger-to-wordpress-redirection' ); ?> <a id="redirection-domain-name-1" href=""></a></strong>
 
 <br/>
 
-Copy template code generated below and paste them in your Blogger.com template. (<a href="https://bloggertowp.org/tutorials/blogger-to-wordpress-redirection-plugin/" target="_blank">How to do this?</a>)<br/>
+<?php esc_html_e( 'Copy template code generated below and paste them in your Blogger.com template.', 'blogger-to-wordpress-redirection' ); ?> (<a href="https://bloggertowp.org/tutorials/blogger-to-wordpress-redirection-plugin/" target="_blank"><?php esc_html_e( 'How to do this?', 'blogger-to-wordpress-redirection' ); ?></a>)<br/>
 
 <span class="description">
-	(<strong>Important:</strong> Do NOT forget to take a backup of old template code.)
+	(<strong><?php esc_html_e( 'Important:', 'blogger-to-wordpress-redirection' ); ?></strong><?php esc_html_e( 'Do NOT forget to take a backup of old template code.', 'blogger-to-wordpress-redirection' ); ?>)
 </span>
 
 <p>
@@ -117,11 +117,11 @@ Copy template code generated below and paste them in your Blogger.com template. 
 
 </p>
 
-After the redirection setup press <b>Verify Configuration</b> button below to test your configuration. <br />When you press the button it will generate a random link for a post on <b id="redirection-domain-name-2"></b>
+<?php esc_html_e( 'After the redirection setup press', 'blogger-to-wordpress-redirection' ); ?> <b><?php esc_html_e( 'Verify Configuration', 'blogger-to-wordpress-redirection' ); ?></b> <?php esc_html_e( 'button below to test your configuration.', 'blogger-to-wordpress-redirection' ); ?> <br /><?php esc_html_e( 'When you press the button it will generate a random link for a post on', 'blogger-to-wordpress-redirection' ); ?> <b id="redirection-domain-name-2"></b>
 
 <div class="submit" style="padding-bottom: 0.5em !important">
 
-	<input type="submit" class="button-primary" onclick = "" name="start" id ="check_config" value="Verify Configuration"/><br />
+	<input type="submit" class="button-primary" onclick = "" name="start" id ="check_config" value="<?php esc_attr_e( 'Verify Configuration', 'blogger-to-wordpress-redirection' ); ?>"/><br />
 
 </div>
 
